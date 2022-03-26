20c929019defb85b609a5c6427d77a0f2f8b30cf
Carson Lloyd <carsonclloyd@gmail.com>
2022-03-25 21:51:14 -0400

    Remove API key.

--- fromSpotifyAPI/get_playlist_info.py
+++ fromSpotifyAPI/get_playlist_info.py
@@ -1,10 +1,11 @@
 from intake import get_playlist_data
 from spotipy.oauth2 import SpotifyClientCredentials
+import config
 
 
 def retrieve_playlist_info(playlist_id='37i9dQZF1DX0kbJZpiYdZl'):
     auth_manager = SpotifyClientCredentials(client_id='6c3b776619c14dba9a9800450215a2f9',
-                                            client_secret='d71b5fcfa94b41f59aba2299893625e5')
+                                            client_secret=config.client_secret)
 
     all_track_data = get_playlist_data(auth_manager, playlist_id)
 
