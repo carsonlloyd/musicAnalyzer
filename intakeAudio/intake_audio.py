3e28bb1ea7f7dabd357979a3bafd6cbec3ea47ca
Carson Lloyd <carsonclloyd@gmail.com>
2022-03-25 21:51:24 -0400

    Start audio processing tree.

---
+++ intakeAudio/intake_audio.py
@@ -0,0 +1,13 @@
+import scipy
+from pyAudioAnalysis import audioBasicIO
+import plotly
+import numpy
+import matplotlib
+import sklearn
+import hmmlearn
+import simplejson
+import eyed3
+import pydub
+
+
+audio = audioBasicIO.read_audio_file('ill_follow_you.m4a')
